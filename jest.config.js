a28e78d4a8e00c659d88ff141bdf5b9519e07445
amrutwali <amrutwali@gmail.com>
2024-07-21 19:50:03 +0530

    change config files to not compile spec files, and test only the spec ts files

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,13 @@
 // jest.config.js
 
 module.exports = {
+	verbose: true,
 	preset: 'ts-jest',
 	testEnvironment: 'node',
-	testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
+	// testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
+	testRegex: '.*\\.spec\\.(ts|tsx|js|jsx)$',
+	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+	transform: {
+		'^.+\\.(ts|tsx)$': 'ts-jest',
+	},
 };
